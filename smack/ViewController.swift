20f478c5a93db573771767cc69ed2134c17d3944
Pedro Paulo Duarte Silva <pedro.paulods@hotmail.com>
2020-11-29 22:58:11 -0300

    Initial Commit

---
+++ smack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  smack
+//
+//  Created by Pedro Paulo on 29/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
